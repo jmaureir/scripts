c9435b252d886bc4431db04bce9931e0054c87bf
Juan-Carlos Maureira_Bravo <jcmaurei@mijote.localdomain>
2009-11-27 13:39:20 +0100

    fixed vector name problem

--- getDataFromReplicatedVector.sh
+++ getDataFromReplicatedVector.sh
@@ -9,24 +9,26 @@ then
     # remove the vector name (directory)
     VECT_FILE=`echo "$file" | cut -d\/ -f 2`
 
-		export VECTOR_FILE_ID=`echo "$VECT_FILE" | cut -d. -f 1 | awk -F- '{ print $(NF) }'`
-		export REPLICA_ID=`echo "$VECT_FILE" | cut -d. -f 1 | awk -F- '{print $(NF-1) }'`
-		export EXPERIMENT=`echo "$VECT_FILE" | cut -d. -f 1 | awk -v Repl=$REPLICA_ID -v Vect=$VECTOR_FILE_ID '{ sub("-"Repl"-"Vect,"",$0);print }'`
-		export VECTOR_NAME=`head -n 1 "$file" | cut -d\" -f 2`
-		export MODULE=`head -n 1 "$file" | cut -d' ' -f 4`
+    export VECTOR_FILE_ID=`echo "$VECT_FILE" | cut -d. -f 1 | awk -F- '{ print $(NF) }'`
+    export REPLICA_ID=`echo "$VECT_FILE" | cut -d. -f 1 | awk -F- '{print $(NF-1) }'`
+    export EXPERIMENT=`echo "$VECT_FILE" | cut -d. -f 1 | awk -v Repl=$REPLICA_ID -v Vect=$VECTOR_FILE_ID '{ sub("-"Repl"-"Vect,"",$0);print }'`
+    export MODULE=`head -n 1 "$file" | cut -d' ' -f 4`
 
-		echo "procesing $file, Module: $MODULE VectorName: $VECTOR_NAME Experiment $EXPERIMENT"
+    export VECTOR_NAME=`head -n 1 "$file" | awk -v l1=$MODULE '{ v=index($0,l1)+length(l1)+1; e=length($0)-v+1; print substr($0,v,e)}'`
+    VECTOR_NAME=`echo $VECTOR_NAME | awk '{ gsub("\"","",$0); print }'`    
 
-		OUTPUT_FILE="$VECTOR_NAME-$MODULE.data"
+    echo "procesing $file, Module: $MODULE VectorName: $VECTOR_NAME Experiment $EXPERIMENT"
+
+    OUTPUT_FILE="$VECTOR_NAME-$MODULE.data"
 	
-		if [ ! -f "$OUTPUT_FILE" ]
-		then
-			echo "Experiment Replica Time Obs" > "$OUTPUT_FILE"
-		fi
-		tail -n +5 "$file" | gawk -F, '{print ENVIRON["EXPERIMENT"],ENVIRON["REPLICA_ID"],$1,$2}' >> "$OUTPUT_FILE"
+    if [ ! -f "$OUTPUT_FILE" ]
+    then
+       echo "Experiment Replica Time Obs" > "$OUTPUT_FILE"
+    fi
+    tail -n +5 "$file" | gawk -F, '{print ENVIRON["EXPERIMENT"],ENVIRON["REPLICA_ID"],$1,$2}' >> "$OUTPUT_FILE"
   done
 else
   echo "usage: $0 {vector_name}"
-	echo "Vector name must be a directory containing one single vector file for each replica"
+  echo "Vector name must be a directory containing one single vector file for each replica"
 fi
 
